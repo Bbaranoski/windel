05e3a98f6bc0167f8fb0778f64d082ba678afc30
Bbaranoski <breno.b.o.baranoski@gmail.com>
2024-12-05 10:58:10 -0300

    test how to create a react component using ts, and make a simple button with usestate

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-
+const App: React.FC = () => {
+  const [count, setCount] = useState<number>(0)
+  return(
+    <div className='App'>
+      <button onClick={() => {
+        setCount(count => count + 1)
+      }}>click</button>
+      <p>{count}</p>
     </div>
-  );
+  )
 }
 
 export default App;
