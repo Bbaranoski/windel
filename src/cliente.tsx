020e1949560c2afdcfecbfc015f1e9ad0425e028
Bbaranoski <breno.b.o.baranoski@gmail.com>
2024-12-06 12:39:13 -0300

    add mutiple scrren on cliente

--- src/cliente.tsx
+++ src/cliente.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Cliente: React.FC = () => {
-    const [tela, setTela] = useState<JSX.Element>(<Consulta />)
-    return <Consulta setTela={setTela} />
+    const [tela, setTela] = useState<JSX.Element>(<></>)
+    useEffect(() => {
+        setTela(<Consulta setTela={setTela}/>)
+    }, [])
+    return <>{tela}</>
 }
 
 interface Props {
@@ -18,17 +21,19 @@ const Consulta: React.FC<Props> = ({ setTela }) => {
             </div>
             <div>
                 <button onClick={() => {
-                    setTela(<Cadastro />)
+                    setTela(<Cadastro setTela={setTela} />)
                 }}>TESTE</button>
             </div>
         </div>
     )
 }
 
-const Cadastro: React.FC = () => {
+const Cadastro: React.FC<Props> = ({ setTela }) => {
     return(
         <div>
-            
+            <button onClick={() => {
+                setTela(<Consulta setTela={setTela} />)
+            }}>Cadastrar</button>
         </div>
     )
 }
