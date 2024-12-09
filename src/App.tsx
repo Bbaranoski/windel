074392bc8a27efa5cf320609410d1f59a4f6ab9d
Bbaranoski <breno.b.o.baranoski@gmail.com>
2024-12-09 08:57:42 -0300

    adding some css on the page and installing the react-icons libary for later

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { useState } from 'react';
 import './App.css';
-import Dashboard from './dashboard';
+import Home from './home';
 import Cliente from './cliente';
 
 const App: React.FC = () => {
 
-  const [tela, setTela] = useState<JSX.Element>(<Dashboard />)
+  const [tela, setTela] = useState<JSX.Element>(<Home />)
   
   return (
     <div className='app'>
       <div className='header'>
-        <h1>WINDEL</h1> 
+        <h1>Sim</h1> 
       </div>
       <div className='side'>
+        <h1>WINDEL</h1>
         <button onClick={() => {
-          setTela(<Dashboard />)
-        }}>dashboard</button>
+          setTela(<Home />)
+        }}>Home</button>
         <button onClick={() => {
           setTela(<Cliente />)
         }}>cliente</button>
